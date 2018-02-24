61a554873ff16831c76d558168b55f3d17f5577e
Jeremy Ong <tjong1998@gmail.com>
2018-02-24 17:18:47 -0500

    gettarget

--- src/oods.py
+++ src/oods.py
@@ -13,6 +13,7 @@ class Player(BasePlayer):
         super().__init__(p_id)  #Initializes the super class. Do not modify!
 
         self.perimeter_nodes = dict()
+        # self.possible_attacks = dict()
 
         """
         Insert player-specific initialization code here
@@ -55,6 +56,18 @@ class Player(BasePlayer):
             if self.is_perimeter(node):
                 self.perimeter_nodes[node] = 0
 
+    def not_me(nodes):
+        return list(filter(lambda x:self.board.nodes[x]['owner'] != self.player_num,nodes))
+
+    def get_target(self):
+        perim_nodes = self.perimeter_nodes.keys()
+        perim_neighbors = []
+        for pn in perim_nodes:
+            perim_neighbors += self.board.neighbors(pn)
+        not_mine_perim_neighbors = self.not_me(perim_neighbors)
+        self.target_node = min(not_mine_perim_neighbors,key=lambda x:len(self.not_me(self.board.neighbors(x))))
+
+
     """
     Called at the start of every placement phase and movement phase.
     """
@@ -65,7 +78,6 @@ class Player(BasePlayer):
         """
 
         self.get_perimeter_nodes()
-        print(list(self.perimeter_nodes.keys()))
         return
 
 
